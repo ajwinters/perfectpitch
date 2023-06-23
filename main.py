5c8dcab8ffd18a804c46a961673317e1ac1aadaf
ajwinters <awin117@gmail.com>
2023-06-23 11:34:40 -0400

    see prior note

--- main.py
+++ main.py
@@ -5,41 +5,36 @@ from PyQt5.QtGui  import *
 from PyQt5.QtWidgets import *
 import chromatic_player
 
-lowO= 2
-highO = 6
+#small change for new branch
+
+lowO= 3
+highO = 5
 Octaves = list(range(lowO,highO+1))
 #Notes = ["A","A#/Bb","B","C","C#/Db","D","D#/Eb","E","F","F#/Gb","G"]
-Notes = ["A","A#","B","C","C#","D","D#","E","F","F#","G"]
-
-
-# class CustomButton(QPushButton):
-#     def __init__(self, text='', parent=None):
-#         super().__init__()
-#         self.button_show()
-
-#     def button_show(self):
-#         button = QPushButton("text",self)
-#         button.clicked.connect(self.on_click)
-
-#     def on_click(self):
-#         print("User Clicked Me")
+Notes = ["C","C#","D","D#","E","F","F#","G","A","A#","B"]
+global glo
+glo=0
 
 class CustomButton(QPushButton):
     def __init__(self, text='',octave='', parent=None):
         self.octave = octave
         self.text = text
         super(QPushButton, self).__init__(text, parent=parent)
-        self.setAcceptDrops(True)
         self.setGeometry(QRect(30, 40, 41, 41))
-        self.setObjectName("btn_a1")
         self.button_show()
+        self.setId = self.text
+        
     
     def button_show(self):
         self.clicked.connect(self.on_click)
 
     def on_click(self):
-        chromatic_player.play_music(r".\mynotes\{}{}.mid".format(self.text,self.octave))
+        chromatic_player.go(int(Notes.index(self.text[:-1]))+12*self.octave)
         print("User Clicked Me")
+
+    def the_button_was_clicked(self):
+        print(self.text)
+        print(glo + 1)
 
 
 class Color(QWidget):
@@ -59,47 +54,25 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("My App")
 
         layout1 = QHBoxLayout()
-        layout2 = QVBoxLayout()
         layout3 = QGridLayout()
-
-        mybut = CustomButton("{}".format(Notes[0]),3)
-
         layout1.setContentsMargins(0,0,0,0)
-        layout1.setSpacing(20)
-
-        layout2.addWidget(Color('red'))
-        layout2.addWidget(Color('yellow'))
-        layout2.addWidget(Color('purple'))
-
-        #layout1.addLayout( layout2 )
-
-        #layout1.addWidget(Color('green'))
+        layout1.setSpacing(5)
+        layout1.addWidget(Color('green'))
         layout3.addWidget(Color('red'))
         layout3.addWidget(Color('purple'))
 
         for j in Octaves:
             layoutTemp = QVBoxLayout()
             for i in Notes:
-                buttontemp = CustomButton("{}{}".format(i,j))
+                buttontemp = CustomButton("{}{}".format(i,j),j)
+                buttontemp.clicked.connect(buttontemp.the_button_was_clicked)
                 layoutTemp.addWidget(buttontemp)
             layout1.addLayout(layoutTemp)
-
-
-
-        layout2.addWidget(mybut)
-
+        
         layout1.addLayout( layout3 )
-
         widget = QWidget()
         widget.setLayout(layout1)
         self.setCentralWidget(widget)
-    
-    def the_button_was_clicked(self):
-        self.button.setText("You already clicked me.")
-        self.button.setEnabled(False)
-
-        # Also change the window title.
-        self.setWindowTitle("My Oneshot App")
 
 app = QApplication(sys.argv)
 w = MainWindow()
