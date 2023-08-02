3abc688fbb9442f6e8ae1ee38d64a160e1bcbdaf
ajwinters <awin117@gmail.com>
2023-08-01 23:24:54 -0400

    Fixing the double note issue through comprimise

--- scratch.py
+++ scratch.py
@@ -7,3 +7,16 @@ import datetime;
 
 
 print(len([1,2,3,4]))
+
+self.mybg.button(0).broadNote.connect(self.check)
+self.mybg.button(1).broadNote.connect(self.check)
+self.mybg.button(2).broadNote.connect(self.check)
+self.mybg.button(3).broadNote.connect(self.check)
+self.mybg.button(4).broadNote.connect(self.check)
+self.mybg.button(5).broadNote.connect(self.check)
+self.mybg.button(6).broadNote.connect(self.check)
+self.mybg.button(7).broadNote.connect(self.check)
+self.mybg.button(8).broadNote.connect(self.check)
+self.mybg.button(9).broadNote.connect(self.check)
+self.mybg.button(10).broadNote.connect(self.check)
+self.mybg.button(11).broadNote.connect(self.check)
