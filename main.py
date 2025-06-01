391488704d24aa4e5b8cb3fc0e315c3dc6d5d20f
Alex Winters <awin117@gmail.com>
2025-06-01 10:56:12 -0400

    Some more cleanup

--- main.py
+++ main.py
@@ -2,53 +2,58 @@ import sys
 import pygame
 import pygame.midi
 import pandas as pd
-import numpy as np
-import time
-from PyQt5.QtCore import *
-from PyQt5.QtGui  import *
-from PyQt5.QtWidgets import *
 import random
-from random import randrange
 import datetime
 import uuid
+from PyQt5.QtCore import QTimer, pyqtSignal
+from PyQt5.QtWidgets import (
+    QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QGridLayout,
+    QPushButton, QButtonGroup, QSpacerItem, QSizePolicy, QWidget
+)
 
-lowO= 2
-highO = 7
-Octaves = list(range(lowO,highO+1))
-Notes = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"]
+# Constants
+LOW_OCTAVE = 2
+HIGH_OCTAVE = 7
+OCTAVES = list(range(LOW_OCTAVE, HIGH_OCTAVE + 1))
+NOTES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 
-random_amount = 3
-random_select = "Random%s" % random_amount
+RANDOM_AMOUNT = 3
+RANDOM_SELECT = f"Random{RANDOM_AMOUNT}"
 
-selection = random_select
-groupsel = {"C Major":["C","D","E","F","G","A","B"],
-            "Standard Tuning":["E","A","D","G","B"],
-            random_select:random.sample(Notes,random_amount)}
+SELECTION = RANDOM_SELECT
+GROUPS = {
+    "All": NOTES,
+    "C Major": ["C", "D", "E", "F", "G", "A", "B"],
+    "Standard Tuning": ["E", "A", "D", "G", "B"],
+    RANDOM_SELECT: random.sample(NOTES, RANDOM_AMOUNT)
+}
 
-grouplist = sorted(groupsel[random_select])
-groupind = [Notes.index(i) for i in grouplist if i in Notes]
+GROUP_LIST = sorted(GROUPS["Standard Tuning"])
+GROUP_INDICES = [NOTES.index(i) for i in GROUP_LIST if i in NOTES]
+SLOTS = len(GROUP_INDICES)
 
-print(grouplist)
-slots = len(groupind)
+MIDI_CHANNEL = 1
+MIDI_VELOCITY = 127
+NOTE_DURATION_MS = 5000
 
-class Player(object):
-    def __init__(self,instrument, *args, **kwargs):
+class Player:
+    """Handles MIDI playback."""
+    def __init__(self, instrument):
         self.instrument = instrument
-        super().__init__(*args, **kwargs)
         pygame.midi.init()
         self.pygame_player = pygame.midi.Output(0)
-        
 
     def play(self, note):
-        self.pygame_player.set_instrument(self.instrument, 1)
-        self.pygame_player.note_on(note, 127, 1)
-        QTimer.singleShot(5000, lambda:
-            self.pygame_player.note_off(note, 127, 1))
+        self.pygame_player.set_instrument(self.instrument, MIDI_CHANNEL)
+        self.pygame_player.note_on(note, MIDI_VELOCITY, MIDI_CHANNEL)
+        QTimer.singleShot(NOTE_DURATION_MS, lambda:
+            self.pygame_player.note_off(note, MIDI_VELOCITY, MIDI_CHANNEL))
 
 class PlayBoardButton(QPushButton):
+    """Button for playing a specific note."""
     playNote = pyqtSignal(int)
     def __init__(self, note, octave, parent=None):
-        text = Notes[note] + str(octave)
+        text = NOTES[note] + str(octave)
         super().__init__(text, parent=parent)
         self.note = note + 12 * octave
         self.clicked.connect(self.emitSignal)
@@ -57,9 +62,10 @@ class PlayBoardButton(QPushButton):
         self.playNote.emit(self.note)
 
 class ChoiceButton(QPushButton):
+    """Button for guessing a note."""
     broadNote = pyqtSignal(int)
     def __init__(self, note, parent=None):
-        text = Notes[note]
+        text = NOTES[note]
         super().__init__(text, parent=parent)
         self.note = note
         self.clicked.connect(self.emitSignal)
@@ -72,30 +78,24 @@ class ChoiceButton(QPushButton):
     def reset(self):
         self.setEnabled(True)
 
-
-
 class MainWindow(QMainWindow):
-
+    """Main application window."""
     def __init__(self):
-        super(MainWindow, self).__init__()
+        super().__init__()
         self.setWindowTitle("Perfect Pitch Training")
-        self.randomrange = randrange(12 * lowO, 12*highO)
         self.tl = []
-        for i in range(lowO,highO):
-            ttl = [12*i+j for j in groupind]
-            self.tl = self.tl + ttl
+        for i in range(LOW_OCTAVE, HIGH_OCTAVE):
+            self.tl += [12 * i + j for j in GROUP_INDICES]
         self.random_note = random.choice(self.tl)
 
         self.correct = None
-        self.pacount = 0 
-
-        self.totalcount = 0 
+        self.pacount = 0
+        self.totalcount = 0
         self.firsttry = 0
         self.firstguess = True
         self.taskid = uuid.uuid1()
 
-
-        self.player = Player(1)    
+        self.player = Player(1)
 
         mainLayout = QVBoxLayout()
         mainLayout.setContentsMargins(0, 0, 0, 0)
@@ -103,15 +103,12 @@ class MainWindow(QMainWindow):
         layoutplay = QHBoxLayout()
         layoutpicks = QHBoxLayout()
         playbutton = QPushButton("Play Again")
-        playbutton.resize(200,100)
         playbutton.setMinimumHeight(50)
         playbutton.setMaximumWidth(100)
         layoutplay.addWidget(playbutton)
         playbutton.clicked.connect(self.playagain)
 
-
         verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Expanding)
-
         mainLayout.addLayout(layoutplay)
         mainLayout.addLayout(layoutpicks)
         mainLayout.addItem(verticalSpacer)
@@ -121,35 +118,22 @@ class MainWindow(QMainWindow):
 
         self.mybg = QButtonGroup(mainLayout)
 
-        for i in groupind:
+        # Choice buttons for guessing
+        for i in GROUP_INDICES:
             buttontemp1 = ChoiceButton(i)
             layoutpicks.addWidget(buttontemp1)
-            self.mybg.addButton(buttontemp1,i)
+            self.mybg.addButton(buttontemp1, i)
 
-            #self.mybg.button(i).pressed.connect(lambda i=i : self.mybg.button(i).changecolor(i,self.random_note))
-            #self.mybg.button(i).released.connect(lambda i=i : self.mybg.button(i).broadNote.connect(self.check))
-        for i in groupind:
+        for i in GROUP_INDICES:
             self.mybg.button(i).broadNote.connect(self.check)
-        # self.mybg.button(0).broadNote.connect(self.check)
-        # self.mybg.button(1).broadNote.connect(self.check)
-        # self.mybg.button(2).broadNote.connect(self.check)
-        # self.mybg.button(3).broadNote.connect(self.check)
-        # self.mybg.button(4).broadNote.connect(self.check)
-        # self.mybg.button(5).broadNote.connect(self.check)
-        # self.mybg.button(6).broadNote.connect(self.check)
-        # self.mybg.button(7).broadNote.connect(self.check)
-        # self.mybg.button(8).broadNote.connect(self.check)
-        # self.mybg.button(9).broadNote.connect(self.check)
-        # self.mybg.button(10).broadNote.connect(self.check)
-        # self.mybg.button(11).broadNote.connect(self.check)
 
-        for j in range(lowO, highO + 1):
+        # Playboard buttons for playing notes
+        for j in range(LOW_OCTAVE, HIGH_OCTAVE + 1):
             for i in range(12):
                 buttontemp = PlayBoardButton(i, j)
                 buttonLayout.addWidget(buttontemp, j, i)
                 buttontemp.playNote.connect(self.player.play)
 
-        
         widget = QWidget()
         widget.setLayout(mainLayout)
         self.setCentralWidget(widget)
@@ -157,19 +141,17 @@ class MainWindow(QMainWindow):
     def playnext(self):
         self.random_note = random.choice(self.tl)
         self.player.play(self.random_note)
-        #print(Notes[self.random_note%12])
 
     def playagain(self):
         self.player.play(self.random_note)
         self.pacount += 1
 
-    def check(self,note):
+    def check(self, note):
         self.guess = note
         self.senddf()
         if self.random_note % 12 == note:
             print("correct")
-            ## reset
-            for i in groupind:
+            for i in GROUP_INDICES:
                 self.mybg.button(i).reset()
             if self.firstguess:
                 self.firsttry += 1
@@ -178,19 +160,27 @@ class MainWindow(QMainWindow):
             self.firstguess = True
             self.taskid = uuid.uuid1()
             print(f"{self.firsttry}/{self.totalcount}")
-
             self.playnext()
         else:
             self.firstguess = False
             self.mybg.button(note).setEnabled(False)
 
     def senddf(self):
-        df = pd.DataFrame({"ID":[self.taskid],"CorrectNote":[self.random_note],"SelectedNote":[self.guess],"PlayAgainCount":[self.pacount],"time":[datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")],"grouping":selection,"Notes":[grouplist],"NotesIndex":[groupind]})
-        df.to_csv(r'.\data\data_v2.csv', mode='a', header=False,index=None)
-        pass
+        df = pd.DataFrame({
+            "ID": [self.taskid],
+            "CorrectNote": [self.random_note],
+            "SelectedNote": [self.guess],
+            "PlayAgainCount": [self.pacount],
+            "time": [datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")],
+            "grouping": SELECTION,
+            "Notes": [GROUP_LIST],
+            "NotesIndex": [GROUP_INDICES]
+        })
+        df.to_csv(r'.\data\data_v2.csv', mode='a', header=False, index=None)
 
-app = QApplication(sys.argv)
-w = MainWindow()
-w.show()
-w.player.play(w.random_note)
-app.exec()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    w.player.play(w.random_note)
+    app.exec()
